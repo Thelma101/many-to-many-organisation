dd8301f89248cc846b2a14c505ea5cbb69afdd04
DCTP-BE <akpata.thelma@gmail.com>
2024-07-09 08:44:50 +0100

    v1

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,7 +1,7 @@
 const { PrismaClient } = require('@prisma/client');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-
+// const { customAlphabet } = require('nanoid');
 const prisma = new PrismaClient();
 const saltRounds = 10;
 const secret = process.env.JWT_SECRET || 'jwt_secret';
@@ -18,11 +18,14 @@ const registerUser = async (req, res) => {
                 statusCode: 400 
             });
         }
+        const { customAlphabet } = await import('nanoid');
+        const nanoid = customAlphabet('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz', 10);
 
         const hashedPassword = await bcrypt.hash(password, saltRounds);
 
         const newUser = await prisma.user.create({
             data: { 
+                userId: nanoid(),
                 firstName, 
                 lastName, 
                 email, 
@@ -34,9 +37,10 @@ const registerUser = async (req, res) => {
         const organisationName = `${firstName}'s Organisation`;
         await prisma.organisation.create({
             data: {
+                orgId: nanoid, 
                 name: organisationName,
                 users: { 
-                    create: [{ userId: newUser.userId }] 
+                    connect: [{ userId: newUser.userId }] 
                 }
             }
         });
@@ -114,3 +118,122 @@ module.exports = {
     registerUser,
     loginUser
 };
+
+
+
+// const { PrismaClient } = require('@prisma/client');
+// const bcrypt = require('bcrypt');
+// const jwt = require('jsonwebtoken');
+
+// const prisma = new PrismaClient();
+// const saltRounds = 10;
+// const secret = process.env.JWT_SECRET || 'jwt_secret';
+
+// const registerUser = async (req, res) => {
+//     const { firstName, lastName, email, password, phone } = req.body;
+
+//     try {
+//         const existingUser = await prisma.user.findUnique({ where: { email } });
+//         if (existingUser) {
+//             return res.status(400).json({ 
+//                 status: 'error', 
+//                 message: 'Email already in use', 
+//                 statusCode: 400 
+//             });
+//         }
+
+//         const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+//         const newUser = await prisma.user.create({
+//             data: { 
+//                 firstName, 
+//                 lastName, 
+//                 email, 
+//                 password: hashedPassword, 
+//                 phone 
+//             }
+//         });
+
+//         const organisationName = `${firstName}'s Organisation`;
+//         await prisma.organisation.create({
+//             data: {
+//                 name: organisationName,
+//                 users: { 
+//                     create: [{ userId: newUser.userId }] 
+//                 }
+//             }
+//         });
+
+//         const accessToken = jwt.sign({ userId: newUser.userId }, secret);
+
+//         res.status(201).json({
+//             status: 'success',
+//             message: 'Registration successful',
+//             data: {
+//                 accessToken,
+//                 user: {
+//                     userId: newUser.userId,
+//                     firstName: newUser.firstName,
+//                     lastName: newUser.lastName,
+//                     email: newUser.email,
+//                     phone: newUser.phone
+//                 }
+//             }
+//         });
+//     } catch (error) {
+//         console.error('Error during registration:', error);
+//         res.status(400).json({ 
+//             status: 'Bad request', 
+//             message: 'Registration unsuccessful', 
+//             statusCode: 400 
+//         });
+//     } finally {
+//         await prisma.$disconnect();
+//     }
+// };
+
+// const loginUser = async (req, res) => {
+//     const { email, password } = req.body;
+
+//     try {
+//         const user = await prisma.user.findUnique({ where: { email } });
+//         if (!user || !(await bcrypt.compare(password, user.password))) {
+//             return res.status(401).json({ 
+//                 status: 'error', 
+//                 message: 'Invalid email or password', 
+//                 statusCode: 401 
+//             });
+//         }
+
+//         const accessToken = jwt.sign({ userId: user.userId }, secret);
+
+//         res.status(200).json({
+//             status: 'success',
+//             message: 'Login successful',
+//             data: {
+//                 accessToken,
+//                 user: {
+//                     userId: user.userId,
+//                     firstName: user.firstName,
+//                     lastName: user.lastName,
+//                     email: user.email,
+//                     phone: user.phone
+//                 }
+//             }
+//         });
+//     } catch (error) {
+//         console.error('Error during login:', error);
+//         res.status(500).json({ 
+//             status: 'error', 
+//             message: 'Login failed', 
+//             statusCode: 500 
+//         });
+//     } finally {
+//         await prisma.$disconnect();
+//     }
+// };
+
+// module.exports = {
+//     registerUser,
+//     loginUser
+// };
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -34,23 +34,12 @@ const getUserById = async (req, res) => {
 
 const updateUser = async (req, res) => {
     const { id } = req.params;
-    const { firstName, lastName, email, phone, password } = req.body;
+    const { firstName, lastName, email, phone } = req.body;
 
     try {
-        const updateData = {};
-
-        if (firstName) updateData.firstName = firstName;
-        if (lastName) updateData.lastName = lastName;
-        if (email) updateData.email = email;
-        if (phone) updateData.phone = phone;
-        if (password) {
-            const hashedPassword = await bcrypt.hash(password, saltRounds);
-            updateData.password = hashedPassword;
-        }
-
         const user = await prisma.user.update({
             where: { userId: id },
-            data: updateData
+            data: { firstName, lastName, email, phone }
         });
 
         res.status(200).json({
@@ -99,3 +88,107 @@ module.exports = {
     updateUser,
     deleteUser,
 };
+
+
+
+// const { PrismaClient } = require('@prisma/client');
+
+// const prisma = new PrismaClient();
+
+// const getUserById = async (req, res) => {
+//     try {
+//         const user = await prisma.user.findUnique({
+//             where: { userId: req.params.id }
+//         });
+
+//         if (!user) {
+//             return res.status(404).json({
+//                 status: 'error',
+//                 message: 'User not found',
+//                 statusCode: 404
+//             });
+//         }
+
+//         res.status(200).json({
+//             status: 'success',
+//             data: { user }
+//         });
+//     } catch (error) {
+//         console.error('Error fetching user:', error);
+//         res.status(500).json({
+//             status: 'Internal server error',
+//             message: 'Failed to fetch user',
+//             statusCode: 500
+//         });
+//     } finally {
+//         await prisma.$disconnect();
+//     }
+// };
+
+// const updateUser = async (req, res) => {
+//     const { id } = req.params;
+//     const { firstName, lastName, email, phone, password } = req.body;
+
+//     try {
+//         const updateData = {};
+
+//         if (firstName) updateData.firstName = firstName;
+//         if (lastName) updateData.lastName = lastName;
+//         if (email) updateData.email = email;
+//         if (phone) updateData.phone = phone;
+//         if (password) {
+//             const hashedPassword = await bcrypt.hash(password, saltRounds);
+//             updateData.password = hashedPassword;
+//         }
+
+//         const user = await prisma.user.update({
+//             where: { userId: id },
+//             data: updateData
+//         });
+
+//         res.status(200).json({
+//             status: 'success',
+//             message: 'User updated successfully',
+//             data: { user }
+//         });
+//     } catch (error) {
+//         console.error('Error updating user:', error);
+//         res.status(400).json({
+//             status: 'Bad request',
+//             message: 'Failed to update user',
+//             statusCode: 400
+//         });
+//     } finally {
+//         await prisma.$disconnect();
+//     }
+// };
+
+// const deleteUser = async (req, res) => {
+//     const { id } = req.params;
+
+//     try {
+//         await prisma.user.delete({
+//             where: { userId: id }
+//         });
+
+//         res.status(200).json({
+//             status: 'success',
+//             message: 'User deleted successfully'
+//         });
+//     } catch (error) {
+//         console.error('Error deleting user:', error);
+//         res.status(400).json({
+//             status: 'Bad request',
+//             message: 'Failed to delete user',
+//             statusCode: 400
+//         });
+//     } finally {
+//         await prisma.$disconnect();
+//     }
+// };
+
+// module.exports = {
+//     getUserById,
+//     updateUser,
+//     deleteUser,
+// };
